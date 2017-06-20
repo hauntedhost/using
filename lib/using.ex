e987643cc2c5999e14c6b553a83f4eb3b04a672c
Sean Omlor <somlor@eml.cc>
2017-06-20 00:08:32 -0400

    initial

---
+++ lib/using.ex
@@ -0,0 +1,18 @@
+defmodule Using do
+  @moduledoc """
+  Documentation for Using.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Using.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/using_test.exs
@@ -0,0 +1,8 @@
+defmodule UsingTest do
+  use ExUnit.Case
+  doctest Using
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
